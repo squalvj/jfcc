e8b332f380cf370e5ec66c0339d94cd86e9ff892
Squalvj <squalvj@gmail.com>
2017-10-31 21:20:29 +0700

    owl-carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,27 @@ $(document).ready(function(){
 	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
 		$(this).toggleClass('open');
 	});
+	var w = $(window).width();
+	var option;
+	if (w >= 993)
+ 		$("#upcoming-right").stick_in_parent();
+
+ 	if (w < 767){
+	 	option = {
+		    center: true,
+		    items:2,
+		    loop:true,
+		    margin:10,
+		}
+	}else{
+		option = {
+		    center: true,
+		    items:2,
+		    loop:true,
+		    margin:10,
+		    autoWidth:true
+	}
+}
+
+ 	$('.owl-carousel').owlCarousel(option);
 });
