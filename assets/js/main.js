ef794918eb215a1cc2a125096cc9268f8699c610
Squalvj <squalvj@gmail.com>
2017-11-01 16:06:53 +0700

    kelar events

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,22 +7,43 @@ $(document).ready(function(){
 	if (w >= 993)
  		$("#upcoming-right").stick_in_parent();
 
- 	if (w < 767){
+ 	if (w < 767 && w > 576 ){
 	 	option = {
 		    center: true,
 		    items:2,
 		    loop:true,
 		    margin:10,
+		    navContainer: '#inside-navigasi-owl',
+		    navText: ["<span class='glyphicon glyphicon-triangle-left' aria-hidden='true'></span>", "<span class='glyphicon glyphicon-triangle-right' aria-hidden='true'></span>"],
+		    dots:true
 		}
-	}else{
+	}
+	else if(w < 576){
 		option = {
 		    center: true,
 		    items:2,
 		    loop:true,
 		    margin:10,
-		    autoWidth:true
+		    navContainer: '#inside-navigasi-owl',
+		    navText: ["<span class='glyphicon glyphicon-triangle-left' aria-hidden='true'></span>", "<span class='glyphicon glyphicon-triangle-right' aria-hidden='true'></span>"],
+		    dots:false
+		}
 	}
-}
+	else{
+		option = {
+
+			    //nav: true,
+			    dots: true,
+			    center: true,
+			    items:2,
+			    loop:true,
+			    margin:10,
+			    autoWidth:true,
+			    navContainer: '#inside-navigasi-owl',
+			    navText: ["<span class='glyphicon glyphicon-triangle-left' aria-hidden='true'></span>", "<span class='glyphicon glyphicon-triangle-right' aria-hidden='true'></span>"]
+		}
+	}
 
  	$('.owl-carousel').owlCarousel(option);
 });
+
